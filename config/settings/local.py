9c031c030877ceb2fc5d1b79227eddde12cab5f0
Abdelrahman <abdelrahmanelsaeid5@gmail.com>
2026-02-13 09:18:31 +0200

    BN-main split django settings

---
+++ config/settings/local.py
@@ -0,0 +1,25 @@
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+from os import path, getenv
+from dotenv import load_dotenv
+from .base import BASE_DIR
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = getenv('SECRET_KEY')
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = getenv('DEBUG')
+
+SITE_NAME = getenv('SITE_NAME')
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
+
+ADMIN_URL = getenv('ADMIN_URL')
+
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = getenv('EMAIL_HOST')
+EMAIL_PORT = getenv('EMAIL_PORT')
+EMAIL_HOST_USER = getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = getenv('DEFAULT_FROM_EMAIL')
+DOMAIN = getenv('DOMAIN')
+MAX_UPLOAD_SIZE = 1024 * 1024 * 1
