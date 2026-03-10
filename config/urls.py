a12abdb60e3691e51f51731d209386c777ba5eb7
Abdelrahman <abdelrahmanelsaeid5@gmail.com>
2026-03-10 22:37:22 +0200

    BN-main add profile functionality

--- config/urls.py
+++ config/urls.py
@@ -33,6 +33,7 @@ urlpatterns = [
     path('api/v1/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path("api/v1/auth/", include("djoser.urls")),
     path("api/v1/auth/", include("core_apps.user_auth.urls")),
+    path("api/v1/profiles/", include("core_apps.user_profile.urls")),
     
 ]
 
